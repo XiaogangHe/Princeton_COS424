9178b9ed23feb71db9401e703b3b2b7301d9ef91
Xiaogang HE <hexg@princeton.edu>
2015-02-27 11:24:04 -0500

    Add BernoulliNB method

--- classifier_NB.py
+++ classifier_NB.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 
-import	numpy			as	np
-import	matplotlib.pyplot	as	plt
-from	sklearn.naive_bayes	import	MultinomialNB
-import	email_process		as	ep
-from	sklearn.metrics		import	accuracy_score
-from	sklearn.feature_selection import SelectKBest
-from	sklearn.feature_selection import chi2
+import	numpy				as	np
+import	matplotlib.pyplot		as	plt
+import	email_process			as	ep
+from	sklearn.naive_bayes		import	MultinomialNB
+from	sklearn.naive_bayes		import	BernoulliNB
+from	sklearn.metrics			import	accuracy_score
+from	sklearn.feature_selection	import	SelectKBest
+from	sklearn.feature_selection	import	chi2
 
 ### For training datasets
 nEmail_tr	= 45000
@@ -14,20 +15,30 @@ emailClass_tr	= file('../HW1/Train/train_emails_classes_200.txt').readlines()
 emailClass_tr	= np.array([str.split(emailClass_tr[i]) for i in range(nEmail_tr)]).squeeze()
 emailClass_tr	= np.where(emailClass_tr=='NotSpam',0,1)
 bow_tr		= ep.read_bagofwords_dat('../HW1/Train/train_emails_bag_of_words_200.dat',nEmail_tr)
+bow_tr_occ	= np.sign(bow_tr)	# Change word counts to word occurence
 
-### Use the Naive Bayes classifier for multinomial models
+### For test datasets
 nEmail_te	= 5000
 emailClass_te	= file('../HW1/Test/test_emails_classes_0.txt').readlines()
 emailClass_te	= np.array([str.split(emailClass_te[i]) for i in range(nEmail_te)]).squeeze()
 emailClass_te	= np.where(emailClass_te=='NotSpam',0,1)
-clf		= MultinomialNB()
-clf.fit(bow_tr,emailClass_tr)
 bow_te		= ep.read_bagofwords_dat('../HW1/Test/test_emails_bag_of_words_0.dat',nEmail_te)
-pre_te		= clf.predict(bow_te)
+bow_te_occ	= np.sign(bow_te)	# Change word counts to word occurence
 
-NB_acu		= accuracy_score(pre_te, emailClass_te)
-print		NB_acu		
+### Naive Bayes classifier for multinomial models
+clf_Mul		= MultinomialNB()
+clf_Mul.fit(bow_tr,emailClass_tr)
+pre_te_Mul	= clf_Mul.predict(bow_te)
+acu_MulNB	= clf_Mul.score(bow_te, emailClass_te)
+print		acu_MulNB		
 
-###
+### Naive Bayes classifier for Bernoulli  models
+clf_Ber		= BernoulliNB()
+clf_Ber.fit(bow_tr_occ,emailClass_tr)
+pre_te_Ber	= clf_Ber.predict(bow_te_occ)
+acu_BerNB	= clf_Ber.score(bow_te_occ, emailClass_te)
+print		acu_BerNB		
+
+### Get the feature index
 X_new		= SelectKBest(chi2, k=200).fit(bow_tr, emailClass_tr).get_support(True)
 print		X_new
