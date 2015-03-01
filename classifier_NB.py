0ba21c6b22d87b768f805031cf4085a30dd2d2ac
Xiaogang HE <hexg@princeton.edu>
2015-03-01 15:21:27 -0500

    Modify NB methods

--- classifier_NB.py
+++ classifier_NB.py
@@ -3,6 +3,7 @@
 import	numpy				as	np
 import	matplotlib.pyplot		as	plt
 import	email_process			as	ep
+from	pandas				import	DataFrame
 from	sklearn.naive_bayes		import	MultinomialNB
 from	sklearn.naive_bayes		import	BernoulliNB
 from	sklearn.metrics			import	accuracy_score
@@ -11,11 +12,15 @@ from	sklearn.feature_selection	import	chi2
 
 ### For training datasets
 nEmail_tr	= 45000
+nWord		= 9579
 emailClass_tr	= file('../HW1/Train/train_emails_classes_200.txt').readlines()
 emailClass_tr	= np.array([str.split(emailClass_tr[i]) for i in range(nEmail_tr)]).squeeze()
 emailClass_tr	= np.where(emailClass_tr=='NotSpam',0,1)
 bow_tr		= ep.read_bagofwords_dat('../HW1/Train/train_emails_bag_of_words_200.dat',nEmail_tr)
 bow_tr_occ	= np.sign(bow_tr)	# Change word counts to word occurence
+
+vocal   	= file('../HW1/Train/train_emails_vocab_200.txt').readlines()
+vocal		= np.array([str.split(vocal[i]) for i in range(nWord)]).squeeze()
 
 ### For test datasets
 nEmail_te	= 5000
@@ -32,7 +37,7 @@ pre_te_Mul	= clf_Mul.predict(bow_te)
 acu_MulNB	= clf_Mul.score(bow_te, emailClass_te)
 print		acu_MulNB		
 
-### Naive Bayes classifier for Bernoulli  models
+### Naive Bayes classifier for Bernoulli models
 clf_Ber		= BernoulliNB()
 clf_Ber.fit(bow_tr_occ,emailClass_tr)
 pre_te_Ber	= clf_Ber.predict(bow_te_occ)
