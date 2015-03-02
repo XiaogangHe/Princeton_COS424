ae5b039a7f63906d00f36fc4e7de9d633ac8f794
Xiaogang HE <hexg@princeton.edu>
2015-03-02 01:12:35 -0500

    Add the function for calibration plot

--- classifier_Library.py
+++ classifier_Library.py
@@ -3,10 +3,30 @@
 import	numpy				as	np
 import	matplotlib.pyplot		as	plt
 import	email_process			as	ep
+import	pandas				as	pd
 from	pandas				import	DataFrame
 from	sklearn.ensemble		import	RandomForestClassifier
 from    sklearn.feature_selection       import  SelectKBest
 from    sklearn.feature_selection       import  chi2
+from	matplotlib 			import	rcParams
+
+dark2_colors = [(0.10588235294117647, 0.6196078431372549, 0.4666666666666667),
+		(0.8509803921568627, 0.37254901960784315, 0.00784313725490196),
+		(0.4588235294117647, 0.4392156862745098, 0.7019607843137254),
+		(0.9058823529411765, 0.1607843137254902, 0.5411764705882353),
+		(0.4, 0.6509803921568628, 0.11764705882352941),
+		(0.9019607843137255, 0.6705882352941176, 0.00784313725490196),
+		(0.6509803921568628, 0.4627450980392157, 0.11372549019607843),
+		(0.4, 0.4, 0.4)]
+
+rcParams['figure.figsize'] = (10, 6)
+rcParams['axes.color_cycle'] = dark2_colors
+rcParams['figure.dpi'] = 150
+rcParams['lines.linewidth'] = 2
+rcParams['axes.grid'] = False
+rcParams['axes.facecolor'] = 'white'
+rcParams['font.size'] = 14
+rcParams['patch.edgecolor'] = 'none'
 
 """
 Read dataset
@@ -45,3 +65,74 @@ Get the new bow (bag of words) matrix according to the selected features
 def	getSelectBOW(data, ind):
 	return	data[:,ind]
 
+"""
+Function
+--------
+calibration_plot
+
+Builds a plot like the one above, from a classifier and review data
+
+Inputs
+-------
+clf :	Classifier object
+A MultinomialNB classifier
+X :	(Nexample, Nfeature) array
+	The bag-of-words data
+Y :	(Nexample) integer array
+1 if a review is Fresh
+"""
+
+def calibration_plot(clf, xtest, ytest):
+	prob = clf.predict_proba(xtest)[:, 1]
+	outcome = ytest
+	data = pd.DataFrame(dict(prob=prob, outcome=outcome))
+	
+	#group outcomes into bins of similar probability
+	bins = np.linspace(0, 1, 20)
+	cuts = pd.cut(prob, bins)
+	binwidth = bins[1] - bins[0]
+	#freshness ratio and number of examples in each bin
+	cal = data.groupby(cuts).outcome.agg(['mean', 'count'])
+	cal['pmid'] = (bins[:-1] + bins[1:]) / 2
+	cal['sig'] = np.sqrt(cal.pmid * (1 - cal.pmid) / cal['count'])
+	#the calibration plot
+	ax = plt.subplot2grid((3, 1), (0, 0), rowspan=2)
+	p = plt.errorbar(cal.pmid, cal['mean'], cal['sig'])
+	plt.plot(cal.pmid, cal.pmid, linestyle='--', lw=1, color='k')
+	plt.ylabel("Empirical P (Spam)")
+	remove_border(ax)
+	#the distribution of P(fresh)
+	ax = plt.subplot2grid((3, 1), (2, 0), sharex=ax)
+	plt.bar(left=cal.pmid - binwidth / 2, height=cal['count'], width=.95 * (bins[1] - bins[0]),
+			fc=p[0].get_color())
+	plt.xlabel("Predicted P (Spam)")
+	remove_border()
+	plt.ylabel("Number")
+	plt.savefig('../HW1/Figures/calibration_plot.pdf', format='PDF')
+
+
+def remove_border(axes=None, top=False, right=False, left=True, bottom=True):
+	
+	"""
+	Minimize chartjunk by stripping out unnecesary plot borders and axis ticks
+	The top/right/left/bottom keywords toggle whether the corresponding plot border is drawn
+	"""
+	
+	ax = axes or plt.gca()
+	ax.spines['top'].set_visible(top)
+	ax.spines['right'].set_visible(right)
+	ax.spines['left'].set_visible(left)
+	ax.spines['bottom'].set_visible(bottom)
+	
+	#turn off all ticks
+	ax.yaxis.set_ticks_position('none')
+	ax.xaxis.set_ticks_position('none')
+	#now re-enable visibles
+	if top:
+		ax.xaxis.tick_top()
+	if bottom:
+		ax.xaxis.tick_bottom()
+	if left:
+		ax.yaxis.tick_left()
+	if right:
+		ax.yaxis.tick_right()
