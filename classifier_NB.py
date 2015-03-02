69990f837baea70f0c4471555d49a5cfb6edbc01
Xiaogang HE <hexg@princeton.edu>
2015-03-01 22:13:18 -0500

    Add feature selection for NB

--- classifier_NB.py
+++ classifier_NB.py
@@ -3,10 +3,10 @@
 import	numpy				as	np
 import	matplotlib.pyplot		as	plt
 import	email_process			as	ep
+import	classifier_Library		as	clfLib
 from	pandas				import	DataFrame
 from	sklearn.naive_bayes		import	MultinomialNB
 from	sklearn.naive_bayes		import	BernoulliNB
-from	sklearn.metrics			import	accuracy_score
 from	sklearn.feature_selection	import	SelectKBest
 from	sklearn.feature_selection	import	chi2
 
@@ -17,7 +17,6 @@ emailClass_tr	= file('../HW1/Train/train_emails_classes_200.txt').readlines()
 emailClass_tr	= np.array([str.split(emailClass_tr[i]) for i in range(nEmail_tr)]).squeeze()
 emailClass_tr	= np.where(emailClass_tr=='NotSpam',0,1)
 bow_tr		= ep.read_bagofwords_dat('../HW1/Train/train_emails_bag_of_words_200.dat',nEmail_tr)
-bow_tr_occ	= np.sign(bow_tr)	# Change word counts to word occurence
 
 vocal   	= file('../HW1/Train/train_emails_vocab_200.txt').readlines()
 vocal		= np.array([str.split(vocal[i]) for i in range(nWord)]).squeeze()
@@ -28,7 +27,18 @@ emailClass_te	= file('../HW1/Test/test_emails_classes_0.txt').readlines()
 emailClass_te	= np.array([str.split(emailClass_te[i]) for i in range(nEmail_te)]).squeeze()
 emailClass_te	= np.where(emailClass_te=='NotSpam',0,1)
 bow_te		= ep.read_bagofwords_dat('../HW1/Test/test_emails_bag_of_words_0.dat',nEmail_te)
-bow_te_occ	= np.sign(bow_te)	# Change word counts to word occurence
+
+### Option for feature selection
+nFeatures	= 200
+feature_select	= False
+
+if feature_select:
+	kbest	= SelectKBest(chi2, k=nFeatures)
+	bow_tr	= kbest.fit_transform(bow_tr, emailClass_tr)
+	bow_te	= kbest.transform(bow_te)
+
+print	bow_te.shape
+print	bow_tr.shape
 
 ### Naive Bayes classifier for multinomial models
 clf_Mul		= MultinomialNB()
@@ -38,12 +48,13 @@ acu_MulNB	= clf_Mul.score(bow_te, emailClass_te)
 print		acu_MulNB		
 
 ### Naive Bayes classifier for Bernoulli models
+bow_tr_occ	= np.sign(bow_tr)	# Change word counts to word occurence
+bow_te_occ	= np.sign(bow_te)	# Change word counts to word occurence
 clf_Ber		= BernoulliNB()
 clf_Ber.fit(bow_tr_occ,emailClass_tr)
 pre_te_Ber	= clf_Ber.predict(bow_te_occ)
 acu_BerNB	= clf_Ber.score(bow_te_occ, emailClass_te)
 print		acu_BerNB		
 
-### Get the feature index
-X_new		= SelectKBest(chi2, k=200).fit(bow_tr, emailClass_tr).get_support(True)
-print		X_new
+
+
